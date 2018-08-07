3f741f716f0836a3124d0df45008bf97a70ef05f
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-08-07 21:39:30 +0200

    Jet lesson 2 part 3 cambio en ojTable añadiendo paginación

--- src/js/viewModels/collComponent.js
+++ src/js/viewModels/collComponent.js
@@ -6,7 +6,7 @@
 /*
  * Your about ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'text!data/treeViewData.json', 'ojs/ojknockout', 'ojs/ojtable', 'ojs/ojarraydataprovider', 'ojs/ojtreeview', 'ojs/ojjsontreedatasource'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'text!data/treeViewData.json', 'ojs/ojknockout', 'ojs/ojtable', 'ojs/ojarraydataprovider', 'ojs/ojtreeview', 'ojs/ojjsontreedatasource', 'ojs/ojpagingcontrol', 'ojs/ojpagingtabledatasource', 'ojs/ojarraytabledatasource'],
     function(oj, ko, $, file) {
 
         function AboutViewModel() {
@@ -24,9 +24,33 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'text!data/treeViewData.json', 'ojs/
                 { DepartmentId: 50, DepartmentName: 'Administration2', LocationId: 200, ManagerId: 300 },
                 { DepartmentId: 60, DepartmentName: 'Marketing3', LocationId: 200, ManagerId: 300 },
                 { DepartmentId: 70, DepartmentName: 'Purchasing4', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 80, DepartmentName: 'Human Resources5', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 90, DepartmentName: 'Human Resources11', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 100, DepartmentName: 'Administration12', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 110, DepartmentName: 'Marketing13', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 120, DepartmentName: 'Purchasing14', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 130, DepartmentName: 'Human Resources15', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 1001, DepartmentName: 'ADFPM 1001 neverending', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 55611, DepartmentName: 'BB', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 1011, DepartmentName: 'Administration', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 2011, DepartmentName: 'Marketing', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 3011, DepartmentName: 'Purchasing', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 4011, DepartmentName: 'Human Resources1', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 5011, DepartmentName: 'Administration2', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 6011, DepartmentName: 'Marketing3', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 7011, DepartmentName: 'Purchasing4', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 8011, DepartmentName: 'Human Resources5', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 9011, DepartmentName: 'Human Resources11', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 10011, DepartmentName: 'Administration12', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 11011, DepartmentName: 'Marketing13', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 12011, DepartmentName: 'Purchasing14', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 13011, DepartmentName: 'Human Resources15', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 100111, DepartmentName: 'ADFPM 1001 neverending', LocationId: 200, ManagerId: 300 },
+                { DepartmentId: 55622, DepartmentName: 'BB', LocationId: 200, ManagerId: 300 },
             ];
 
-            self.dataprovider = new oj.ArrayDataProvider(deptArray, { keyAttributes: 'DepartmentId', implicitSort: [{ attribute: 'DepartmentId', direction: 'ascending' }] });
+            // self.dataprovider = new oj.ArrayDataProvider(deptArray, { keyAttributes: 'DepartmentId', implicitSort: [{ attribute: 'DepartmentId', direction: 'ascending' }] });
+            self.dataprovider = new oj.PagingTableDataSource(new oj.ArrayTableDataSource(deptArray, { idAttribute: 'DepartmentId' }));
 
 
 
